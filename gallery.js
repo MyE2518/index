59b43769e5889d65167fc7f856d3fd7580e0c476
MyE2518 <emolinapicharde@gmail.com>
2025-11-05 10:16:42 -0300

    Add files via upload

---
+++ gallery.js
@@ -0,0 +1,14 @@
+
+//login
+//--------------
+function login(){
+    const user=document.getElementById('usuario').value;
+}
+if (user === usuario){
+    user= document.getElementById('ingreso')=styledisplay='none';
+    user= document.getElementById('tapa')=styledisplay='none';
+}
+
+else{
+  alert("usuario incoerrecto")
+}
